6450c0f76eb9e39ebf4eb90f09f0f6deb4abae68
athena3764 <jhhsu11@gmail.com>
2017-03-28 18:08:05 -0700

    Update water.c

--- water.c
+++ water.c
@@ -4,23 +4,18 @@
 /*Implement a program that reports a user’s water usage, 
 converting minutes spent in the shower to bottles of drinking water.*/
 
-int main(void){
-    
+int main(void)
+{
     int minutes;
     int bottles;
-    do { 
-        
+    do 
+    { 
     printf("Enter minutes spent in the shower: ");
-    
     minutes = GetInt();
-   
-    } while (minutes < 0);
+    } 
+    while (minutes < 0);
     
     /*12 bottles of water used per minute of shower*/
-    
     bottles = minutes * 12;
-   
-    printf("Bottles of water used in shower: %i\n", bottles);
-    
-    
+    printf("Bottles of water used in shower: %i\n", bottles);  
 }
