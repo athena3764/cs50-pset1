8535b45bb408832060f2cf01522ad9ccc764833f
athena3764 <jhhsu11@gmail.com>
2017-03-29 15:16:39 -0700

    Update mario.c

--- mario.c
+++ mario.c
@@ -4,7 +4,7 @@
 /* Write a program that creates a half-pyramid using hashes (#) for blocks*/
 int main(void)
 {
-  int num;
+        int num;
   do {
     printf("Enter half-pyramid’s height, an non-negative integer no greater than 23: ");
     num = GetInt();
